9f72489f5c743f22c2cc42d589dc1947b366e13d
Ricardo Alves <ricardospalves@gmail.com>
2023-09-27 15:15:07 -0300

    feat(books/api): add cors

--- books-api/api/src/app.ts
+++ books-api/api/src/app.ts
@@ -1,8 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 import { router } from './routes/booksRouter'
 
 const app = express()
 
+app.use(
+  cors({
+    origin: '*',
+  }),
+)
 app.use(express.json())
 
 app.use(

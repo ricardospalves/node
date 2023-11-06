14dd6f9cf8e6848e981d0647d032985ccdc2d7d2
Ricardo Alves <ricardospalves@gmail.com>
2023-11-06 17:45:17 -0300

    fix(login/web): remove autoFocus from password

--- login/web/src/page/login/form/form.tsx
+++ login/web/src/page/login/form/form.tsx
@@ -6,7 +6,7 @@ export const Form = () => {
     <form className="grid gap-4">
       <Field label="E-mail" type="email" autoFocus />
 
-      <Field label="Senha" type="password" autoFocus />
+      <Field label="Senha" type="password" />
 
       <Button type="submit" className="flex w-full">
         Entrar

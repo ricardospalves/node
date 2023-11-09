875f29a6f2d2d20a57b1c9ec482b12440b92d49f
Ricardo Alves <ricardospalves@gmail.com>
2023-11-09 16:39:44 -0300

    feat(login/web): add bearer token to api request

--- login/web/src/services/api/api.service.ts
+++ login/web/src/services/api/api.service.ts
@@ -1,6 +1,25 @@
 import axios from 'axios'
 
-export const api = axios.create({
+const isServer = typeof window === 'undefined'
+
+const api = axios.create({
   baseURL: 'http://localhost:3333',
   withCredentials: true,
 })
+
+api.interceptors.request.use(async (config) => {
+  if (isServer) {
+    const { cookies } = await import('next/headers')
+    const tokenCookie = cookies().get('token')
+
+    if (tokenCookie?.value) {
+      config.headers.Authorization = `Bearer ${tokenCookie.value}`
+    }
+
+    return config
+  }
+
+  return config
+})
+
+export { api }

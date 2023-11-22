6e31e3ee0941a44d3a320083fab8e03f625abbc2
Ricardo Alves <ricardospalves@gmail.com>
2023-11-22 16:46:16 -0300

    refactor(login/web): add link to Header logo

--- login/web/src/components/header/header.tsx
+++ login/web/src/components/header/header.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link'
 import { UserMenu } from './user-menu'
 
 export const Header = () => {
   return (
-    <header className="p-2 shrink-0 sticky top-0 border-b">
+    <header className="px-2 shrink-0 sticky top-0 border-b bg-black">
       <div className="flex items-center justify-between max-w-5xl w-full mx-auto">
-        <h1 className="uppercase font-bold">Logo</h1>
+        <Link href="/" className="flex items-center uppercase font-bold h-16">
+          Logo
+        </Link>
 
         <UserMenu />
       </div>
